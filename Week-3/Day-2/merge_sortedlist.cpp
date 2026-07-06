033af438a8e1cb52784b5208eae81e0fe1273d7a
urvarshney111-coder <urvarshney111@gmail.com>
2026-07-06 14:21:59 +0530

    Solve Merge Two Sorted Lists using iterative approach

---
+++ Week-3/Day-2/merge_sortedlist.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
+
+    while (list1 != NULL && list2 != NULL) {
+        if (list1->val <= list2->val) {
+            tail->next = list1;
+            list1 = list1->next;
+        } else {
+            tail->next = list2;
+            list2 = list2->next;
+        }
+        tail = tail->next;
+    }
+
+    if (list1 != NULL)
+        tail->next = list1;
+    else
+        tail->next = list2;
+
+    return dummy.next;
+}
+
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    ListNode* list1 = new ListNode(1);
+    list1->next = new ListNode(2);
+    list1->next->next = new ListNode(4);
+
+    ListNode* list2 = new ListNode(1);
+    list2->next = new ListNode(3);
+    list2->next->next = new ListNode(4);
+
+    ListNode* merged = mergeTwoLists(list1, list2);
+
+    cout << "Merged List: ";
+    printList(merged);
+
+    return 0;
+}
