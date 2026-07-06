91849c95f0c8cc311265d80f0f02bb4b24ae6c05
urvarshney111-coder <urvarshney111@gmail.com>
2026-07-06 14:22:19 +0530

    Solve Remove Nth Node From End of List using two pointers

---
+++ Week-3/Day-2/remove_nth_node_last.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+    ListNode dummy(0);
+    dummy.next = head;
+
+    ListNode* fast = &dummy;
+    ListNode* slow = &dummy;
+
+    for (int i = 0; i <= n; i++) {
+        fast = fast->next;
+    }
+
+    while (fast != NULL) {
+        fast = fast->next;
+        slow = slow->next;
+    }
+
+    ListNode* temp = slow->next;
+    slow->next = slow->next->next;
+    delete temp;
+
+    return dummy.next;
+}
+
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    int n = 2;
+
+    head = removeNthFromEnd(head, n);
+
+    cout << "Updated List: ";
+    printList(head);
+
+    return 0;
+}
