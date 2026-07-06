fa50008b9577584c335a893179da70fc0d1063ee
urvarshney111-coder <urvarshney111@gmail.com>
2026-07-06 14:25:51 +0530

    Solve Sliding Window Maximum using deque

---
+++ Week-3/Day-5/sliding_window_maximum.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+using namespace std;
+
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    deque<int> dq;
+    vector<int> ans;
+
+    for (int i = 0; i < nums.size(); i++) {
+        while (!dq.empty() && dq.front() <= i - k)
+            dq.pop_front();
+
+        while (!dq.empty() && nums[dq.back()] < nums[i])
+            dq.pop_back();
+
+        dq.push_back(i);
+
+        if (i >= k - 1)
+            ans.push_back(nums[dq.front()]);
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {1, 3, -1, -3, 5, 3, 6, 7};
+    int k = 3;
+
+    vector<int> result = maxSlidingWindow(nums, k);
+
+    cout << "Sliding Window Maximums: ";
+    for (int x : result)
+        cout << x << " ";
+
+    cout << endl;
+
+    return 0;
+}
