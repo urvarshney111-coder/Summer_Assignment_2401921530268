ea74715f40da3f97b1606a1072fea9a6a2a3599e
urvarshney111-coder <urvarshney111@gmail.com>
2026-07-06 14:05:51 +0530

    Implement reverse linked list function

---
+++ Week-3/Day-1/reverse_linkedlist.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+ListNode* reverseList(ListNode* head) {
+    ListNode* prev = NULL;
+    ListNode* curr = head;
+
+    while (curr != NULL) {
+        ListNode* nextNode = curr->next; 
+        curr->next = prev;               
+        prev = curr;                     
+        curr = nextNode;                 
+    }
+
+    return prev;
+}
+
+
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original List: ";
+    printList(head);
+
+    head = reverseList(head);
+
+    cout << "Reversed List: ";
+    printList(head);
+
+    return 0;
+}
